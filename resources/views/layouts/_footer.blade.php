617ad7469898b904c615e6407392c7c1d1a0deb4
john <xuyun5566@gmail.com>
2019-06-10 10:09:52 +0000

    命名路由

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,9 +1,10 @@
 <footer class="footer">
-    <img src="https://iocaffcdn.phphub.org//uploads/communities/WtC3cPLHzMbKRSZnagU9.png" class="brand-icon">
+    <img src="https://iocaffcdn.phphub.org//uploads/communities/WtC3cPLHzMbKRSZnagU9.png
+" class="brand-icon">
     <a href="https://laravel-china.org/courses" target="_blank">
         刻意练习 每日精进
     </a>
-    <div class="float-right"><a href="/about">关于</a>
+    <div class="float-right"><a href="{{route('about')}}}">关于</a>
     </div>
 {{--    <img class="brand-icon" src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj1>--}}
 {{--  <a href= target=_blank>--}}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/','StaticPagesController@home');
-Route::get('/help','StaticPagesController@help');
-Route::get('/about','StaticPagesController@about');
+use Illuminate\Support\Facades\Route;
+
+Route::get('/','StaticPagesController@home')->name('home');
+Route::get('/help','StaticPagesController@help')->name('help');
+Route::get('/about','StaticPagesController@about')->name('about');
