a2e82c1cbc9d71b7f5b16a5bdfd5c4a08ecf0ed8
john <xuyun5566@gmail.com>
2019-06-10 08:53:11 +0000

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.default')
 @section('title','主页')
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+<h1>Hello Laravel</h1>
+        <p class="lead">
+            你现在看到的是<a href="https://laravel-china.org/courses/laravel-essential-tra"></a>
+        </p>
+        <p>
+            一切从这里开始
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+        </p>
+    </div>
 @stop
